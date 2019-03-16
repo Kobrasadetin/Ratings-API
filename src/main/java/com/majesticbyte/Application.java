611e06a60afe0e6391b429c3c594ae7f5a06c08d
Kobrasadetin <mikko.karjanmaa@gmail.com>
2019-03-16 19:49:44 +0200

    setup: initialize project files

---
+++ src/main/java/com/majesticbyte/Application.java
@@ -0,0 +1,10 @@
+package com.majesticbyte;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
